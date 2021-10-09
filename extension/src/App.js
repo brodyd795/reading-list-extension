ce74dd3ab36d638a7363307a0aba117becd4adc0
Brody Dingel <brodydingel795@gmail.com>
2021-10-09 18:54:21 -0500

    Refactor dotenv import

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import { changeColor } from './main';
+import dotenv from "dotenv";
+
+import "./App.css";
+import { changeColor } from "./main";
+
+dotenv.config();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={changeColor}>
-          Change color
-        </button>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<button onClick={changeColor}>Change color</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;

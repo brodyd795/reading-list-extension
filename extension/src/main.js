18b90a5a7bd75726e46cb62911aa4c0e29acc124
Brody Dingel <brodydingel795@gmail.com>
2021-10-10 06:08:39 -0500

    Add fetch to reading-list api

--- extension/src/main.js
+++ extension/src/main.js
@@ -1,12 +1,19 @@
 /*global chrome*/
 
 export async function addToReadingList() {
-	try {
-		await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			console.log("adding to reading list: ", tabs[0].url);
+	let success = true;
+
+	await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		const url = tabs[0].url;
+		const key = process.env.REACT_APP_API_SECRET;
+
+		fetch("http://localhost:3000/api/controllers/reading-list", {
+			method: "POST",
+			body: JSON.stringify({ url, key }),
+		}).catch(() => {
+			success = false;
 		});
-		return true;
-	} catch (error) {
-		return false;
-	}
+	});
+
+	return success;
 }

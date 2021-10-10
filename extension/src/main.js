2adadd1669c9ef4dd840522f0e501c7dd89f6209
Brody Dingel <brodydingel795@gmail.com>
2021-10-10 05:46:56 -0500

    Say tabs instead

--- extension/src/main.js
+++ extension/src/main.js
@@ -2,12 +2,11 @@
 
 export async function addToReadingList() {
 	try {
-		await chrome.tabs.query({ active: true, currentWindow: true }, (r) => {
-			console.log("adding to reading list: ", r[0].url);
+		await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			console.log("adding to reading list: ", tabs[0].url);
 		});
 		return true;
 	} catch (error) {
-		console.log(`error`, error);
 		return false;
 	}
 }
